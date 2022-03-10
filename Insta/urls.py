265eb0868b21b419c9a83d1613da0b688ce539f9
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-10 21:05:30 +0300

    add cloudinary

--- Insta/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import  include
-from django.contrib.auth import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('instagram.urls')),
-]
