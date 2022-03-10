ef19c236f76fdf87ccf4b28ea65a02095dd4e8e0
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-10 19:23:13 +0300

    edit urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,17 +3,17 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns=[
-       #path('',views.welcome,name = 'welcome'),
-    path('',views.index,name='index'),
-     path('profile/(\d+)',views.profile,name='profile'),
-    path('profile/update/(\d+)',views.update_profile,name='update_profile'),
-    path('no profile/(\d+)',views.no_profile, name='no_profile'),
+    
+    path('',views.index, name='index'),
+    path('profile/',views.profile, name='profile'),
+    path('profile/update/)',views.update_profile, name='update_profile'),
+    path('no profile/()',views.no_profile, name='no_profile'),
     path('search/', views.search_results, name='search_results'),
-    path('new/image/(\d+)',views.new_image,name="new_image"),
-    path('comment/(\d+)', views.comment, name='comment'),
-    path('comment/like/(\d+)',views.like_pic,name="like_pic"),
-    path('image/update/(\d+)',views.update_image,name='update_image'),
-    path('profile/follow/(\d+)', views.follow, name='follow'),
+    path('new/image/',views.new_image, name="new_image"),
+    path('comment/', views.comment, name='comment'),
+    path('comment/like/',views.like_pic, name="like_pic"),
+    path('image/update/',views.update_image, name='update_image'),
+    path('profile/follow/', views.follow, name='follow'),
     ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
